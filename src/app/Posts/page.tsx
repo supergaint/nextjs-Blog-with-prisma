466e6e8378c47be3ea9f07f186306c4c926a2b2b
Muhammad Ubaid <ku83799@gmail.com>
2024-12-03 11:21:10 +0500

    added pages no prisma yet

---
+++ src/app/Posts/page.tsx
@@ -0,0 +1,7 @@
+ export default async function PostsPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center text-center">
+      <h1 className="text-3xl font-semibold">Posts (0)</h1>
+    </div>
+  );
+}
