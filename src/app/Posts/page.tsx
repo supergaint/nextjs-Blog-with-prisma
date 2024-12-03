5ff42fe7fe2e1f855e388ad3ebf244f090e6a1fe
Muhammad Ubaid <ku83799@gmail.com>
2024-12-03 15:42:07 +0500

    intialaized the prisma and the data base

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -2,6 +2,8 @@
   return (
     <div className="flex min-h-screen flex-col items-center justify-center text-center">
       <h1 className="text-3xl font-semibold">Posts (0)</h1>
+
+      <ul className="border-t border-b border"></ul>
     </div>
   );
 }
